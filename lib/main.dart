031525cf4e03d8f3c7123cafc041e0ff47419101
Lavanya Thangaraj <lavanyathangaraj789@gmail.com>
2025-10-07 18:38:21 -0400

    Add Halloween characters or objects

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(HalloweenStoryApp());
@@ -40,7 +41,7 @@ class IntroScreen extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Image.asset(
-                'assets/images/halloween.webp', 
+                'assets/images/halloween1.webp',
                 height: 250,
                 fit: BoxFit.contain,
               ),
@@ -65,7 +66,7 @@ class IntroScreen extends StatelessWidget {
                 onTap: () {
                   Navigator.push(
                     context,
-                    MaterialPageRoute(builder: (_) => const NextPage()),
+                    MaterialPageRoute(builder: (_) => const SpookyPage()),
                   );
                 },
                 child: Container(
@@ -85,20 +86,118 @@ class IntroScreen extends StatelessWidget {
   }
 }
 
-class NextPage extends StatelessWidget {
-  const NextPage({super.key});
+class SpookyPage extends StatefulWidget {
+  const SpookyPage({super.key});
+
+  @override
+  State<SpookyPage> createState() => _SpookyPageState();
+}
+
+class _SpookyPageState extends State<SpookyPage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  final Random _random = Random();
+
+  List<Offset> _positions = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _generateInitialPositions();
+    _controller =
+        AnimationController(vsync: this, duration: const Duration(seconds: 5))
+          ..addListener(() {
+            if (_controller.status == AnimationStatus.completed) {
+              _generateNewPositions();
+              _controller.repeat();
+            }
+            setState(() {});
+          })
+          ..repeat();
+  }
+
+  void _generateInitialPositions() {
+    _positions = List.generate(6, (_) {
+      return Offset(
+        _random.nextDouble(),
+        _random.nextDouble(),
+      );
+    });
+  }
+
+  void _generateNewPositions() {
+    _positions = List.generate(6, (_) {
+      return Offset(
+        _random.nextDouble(),
+        _random.nextDouble(),
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  Widget _animatedObject(int index, String asset, double size) {
+    final dx = _positions[index].dx;
+    final dy = _positions[index].dy;
+
+    return AnimatedPositioned(
+      duration: const Duration(seconds: 5),
+      curve: Curves.easeInOut,
+      left: dx * MediaQuery.of(context).size.width,
+      top: dy * MediaQuery.of(context).size.height,
+      child: Opacity(
+        opacity: 0.8 + 0.2 * sin(_controller.value * 2 * pi),
+        child: Image.asset(asset, width: size),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(title: const Text('Next Page')),
-      body: const Center(
-        child: Text(
-          'Start your spooky story here!',
-          style: TextStyle(color: Colors.orangeAccent, fontSize: 20),
-          textAlign: TextAlign.center,
-        ),
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Container(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Colors.black, Colors.deepPurple],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                ),
+              ),
+            ),
+          ),
+          Stack(
+            children: [
+              _animatedObject(0, 'assets/images/bat.png', 80),
+              _animatedObject(1, 'assets/images/bat.png', 70),
+              _animatedObject(2, 'assets/images/pumpkin.jpeg', 90),
+              _animatedObject(3, 'assets/images/pumpkin.jpeg', 100),
+              _animatedObject(4, 'assets/images/owl.png', 85),
+              _animatedObject(5, 'assets/images/ghost1.png', 95),
+            ],
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: Text(
+              "🦇 TRAPSSSS!! 🦇",
+              style: TextStyle(
+                color: Colors.orangeAccent.withOpacity(0.9),
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                shadows: [
+                  Shadow(blurRadius: 15, color: Colors.orangeAccent),
+                ],
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ],
       ),
     );
   }
