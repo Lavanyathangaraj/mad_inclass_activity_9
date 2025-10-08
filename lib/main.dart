aadd3e679e8994651bf9a18174d5e3deaa9d46a3
Lavanya Thangaraj <lavanyathangaraj789@gmail.com>
2025-10-07 23:42:54 -0400

    Background music

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() {
   runApp(HalloweenStoryApp());
@@ -96,44 +97,112 @@ class SpookyPage extends StatefulWidget {
 class _SpookyPageState extends State<SpookyPage> with SingleTickerProviderStateMixin {
   late AnimationController _controller;
   final Random _random = Random();
-
+  final AudioPlayer _bgmPlayer = AudioPlayer();
+  final AudioPlayer _effectPlayer = AudioPlayer();
   late List<_SpookyObject> _objects;
 
   @override
   void initState() {
     super.initState();
-
     _controller = AnimationController(
       vsync: this,
-      duration: const Duration(seconds: 10),
+      duration: const Duration(seconds: 75),
     )..repeat(reverse: true);
 
     _objects = [
-      _SpookyObject('assets/images/bat.png', 80),
-      _SpookyObject('assets/images/bat.png', 60),
-      _SpookyObject('assets/images/pumpkin.jpeg', 100),
-      _SpookyObject('assets/images/owl.png', 90),
-      _SpookyObject('assets/images/ghost1.png', 120),
+      _SpookyObject('assets/images/bat.png', 80, isTrap: true),
+      _SpookyObject('assets/images/owl.png', 90, isTrap: true),
+      _SpookyObject('assets/images/bat.png', 60, isTrap: false),
+      _SpookyObject('assets/images/ghost1.png', 120, isTrap: false),
+      _SpookyObject('assets/images/pumpkin.jpeg', 100, isTrap: false, isWinningItem: true),
     ];
+    _playBackgroundMusic();
+  }
+
+  Future<void> _playBackgroundMusic() async {
+    await _bgmPlayer.setReleaseMode(ReleaseMode.loop);
+    await _bgmPlayer.play(AssetSource('sounds/bg.mp3'));
+  }
+
+  Future<void> _playEffect(String sound) async {
+    await _effectPlayer.play(AssetSource('sounds/$sound'));
   }
 
   Widget _buildSpookyObject(_SpookyObject obj, int index, Size size) {
-    final double dx = sin((_controller.value * 2 * pi) + index) * 100 +
+    final double dx = sin((_controller.value * 0.4 * pi) + index) * 100 +
         _random.nextDouble() * size.width * 0.8;
-    final double dy = cos((_controller.value * 2 * pi) + index) * 60 +
+    final double dy = cos((_controller.value * 0.4 * pi) + index) * 60 +
         _random.nextDouble() * size.height * 0.8;
+    final double rotationAngle = _controller.value * 0.02 * pi;
 
     return Positioned(
       left: dx,
       top: dy,
-      child: Transform.rotate(
-        angle: _controller.value * 2 * pi,
-        child: Opacity(
-          opacity: 0.6 + 0.4 * sin(_controller.value * 2 * pi),
-          child: Image.asset(obj.asset, width: obj.size),
+      child: GestureDetector(
+        onTap: () {
+          if (obj.isWinningItem) {
+            _playEffect('safe.mp3');
+            _showWinningDialog();
+          } else if (obj.isTrap) {
+            _playEffect('trap.mp3');
+            _flashRed(obj);
+          }
+        },
+        child: AnimatedBuilder(
+          animation: _controller,
+          builder: (_, __) => Transform.rotate(
+            angle: rotationAngle,
+            child: Opacity(
+              opacity: 0.6 + 0.4 * sin(_controller.value * 2 * pi),
+              child: Image.asset(obj.asset, width: obj.size),
+            ),
+          ),
         ),
       ),
     );
+  }
+
+  void _flashRed(_SpookyObject obj) async {
+    OverlayEntry? overlay;
+    overlay = OverlayEntry(
+      builder: (_) => Positioned.fill(
+        child: Container(color: Colors.red.withOpacity(0.4)),
+      ),
+    );
+    Overlay.of(context).insert(overlay);
+    await Future.delayed(const Duration(milliseconds: 300));
+    overlay.remove();
+  }
+
+  void _showWinningDialog() {
+    showDialog(
+      context: context,
+      builder: (_) => AlertDialog(
+        backgroundColor: Colors.deepPurple[900],
+        title: const Text(
+          "🎉 You Found It! 🎃",
+          style: TextStyle(color: Colors.orangeAccent),
+        ),
+        content: const Text(
+          "The spirits are pleased with your bravery!",
+          style: TextStyle(color: Colors.white70),
+        ),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(context),
+            child: const Text("Continue", style: TextStyle(color: Colors.orangeAccent)),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    _bgmPlayer.dispose();
+    _effectPlayer.dispose();
+    super.dispose();
   }
 
   @override
@@ -181,16 +250,12 @@ class _SpookyPageState extends State<SpookyPage> with SingleTickerProviderStateM
       ),
     );
   }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
 }
 
 class _SpookyObject {
   final String asset;
   final double size;
-  _SpookyObject(this.asset, this.size);
+  final bool isTrap;
+  final bool isWinningItem;
+  _SpookyObject(this.asset, this.size, {this.isTrap = false, this.isWinningItem = false});
 }
